42c20dc8540feb60f0357f101ed926b3d9105a58
Ari <arianiapalmer@gmail.com>
2022-05-29 01:36:58 -0400

    cleaning up a bit

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ let firstName = prompt("Hey! What's your name?");
     document.write("Wassup " + firstName + ", thanks for checking out my site! ")
 
 if (firstName == "Kassie") {
-    document.write("Wassup " + firstName + " thanks for checking out my site! HOPE YOU LIKE IT :)");
+    document.write("Thanks for checking out my site! HOPE YOU LIKE IT :)");
 } else if (firstName == "Kahdija") {
     document.write("Love you bestie :)");
 }
+let favAnime = Prompt("whats youre favorite anime")
