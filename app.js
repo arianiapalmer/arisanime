1e0dfd3f54d53307e2b65cdc4fbed0979b32a026
Ari <arianiapalmer@gmail.com>
2022-05-29 01:49:29 -0400

    added another dynamic component

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let firstName = prompt("Hey! What's your name?");
     console.log(firstName);
-    document.write("Wassup " + firstName + ", thanks for checking out my site! ")
+    document.write("Wassup " + firstName + ", thanks for checking out my site! ");
 
 if (firstName == "Kassie") {
     document.write("Thanks for checking out my site! HOPE YOU LIKE IT :)");
 } else if (firstName == "Kahdija") {
     document.write("Love you bestie :)");
 }
-let favAnime = Prompt("whats youre favorite anime")
+
+let favAnime = prompt("whats your favorite anime");
+    document.write(" Your favorite anime is " + favAnime);
+    if (favAnime == "naruto") {
+        document.write(", same here!");
+    }
