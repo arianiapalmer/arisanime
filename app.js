7437ee8a5bc16a2fc68d6cf5c8985c9c88c348e0
Ari <arianiapalmer@gmail.com>
2022-05-30 17:59:51 -0400

    finally figured out function kinda

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
-let firstName = prompt("Hey! What's your name?");
-    console.log(firstName);
-    document.write("Wassup " + firstName + ", thanks for checking out my site! ");
+//let firstName = prompt("Hey! What's your name?");
+  // console.log(firstName);
+//    document.write("Wassup " + firstName + ", thanks for checking out my site! ");
 
-if (firstName == "Kassie") {
-    document.write("Thanks for checking out my site! HOPE YOU LIKE IT :)");
-} else if (firstName == "Kahdija") {
+//if (firstName == "Kassie") {
+//    document.write("Thanks for checking out my site! HOPE YOU LIKE IT :)");
+//} else if (firstName == "Kahdija") {
+//    document.write("Love you bestie :)");
+//}
+
+//let favAnime = prompt("whats your favorite anime");
+//    document.write(" Your favorite anime is " + favAnime);
+//    if (favAnime == "naruto") {
+//        document.write(", same here!");
+
+let firstName = "";
+function askName() {
+    let firstName = prompt("Hey! What's your name?");
+    if (firstName == "Kassie") {
+        document.write("Hi " + firstName + "! Thanks for checking out my site! HOPE YOU LIKE IT :)");
+    } else if (firstName == "Kahdija") {
     document.write("Love you bestie :)");
+    } else {
+     console.log(firstName);
+    document.write("Wassup " + firstName + ", thanks for checking out my site! ");
+    }
 }
 
-let favAnime = prompt("whats your favorite anime");
-    document.write(" Your favorite anime is " + favAnime);
-    if (favAnime == "naruto") {
-        document.write(", same here!");
+function whatAnime() {
+    let whatAnime = prompt("Of Naruto, One Piece, and Bleach, which anime is your favorite?");
+    if (whatAnime == "Naruto"){
+        document.write("Interesting. Are you an imperialist? Lol jk, it is good af.");
     }
-
+    else if (whatAnime == "One Piece") {
+        document.write("Nice! I commend you for watching all of those episdoes");
+    }
+    else if (whatAnime == "Bleach") {
+        document.write("Personally I don't get the hype, but the soundtrack is fuego");
+    }
+    else {
+        document.write("Sorry that was not one of the choices! Please try again.")
+    }
+}
+//let favAnime = "";
+//function whatAnime() {
+//    let favAnime = prompt ("What's your favorite anime?")\
+//    if (favAnime == "naruto"){
+//        document.write("Interesting. Are you pro imperialism?")
+//    }
+//}
