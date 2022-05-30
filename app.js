edb1a9a5cbf794cb9d3e83bf74673b0ccb682c5c
Ari <arianiapalmer@gmail.com>
2022-05-30 19:26:58 -0400

    added while loop

--- app.js
+++ app.js
@@ -27,21 +27,28 @@ function askName() {
 }
 
 function whatAnime() {
-    let urAnime = prompt("Of Naruto, One Piece, and Bleach, which anime is your favorite?");
-    if (urAnime == "Naruto"){
-        document.write("Interesting. Are you an imperialist? Lol jk, it is good af.");
-    }
-    else if (urAnime == "One Piece") {
-        document.write("Nice! I commend you for watching all of those episdoes");
-    }
-    else if (urAnime == "Bleach") {
-        document.write("Personally I don't get the hype, but the soundtrack is fuego");
-    }
-    else {
-        alert("Sorry that was not one of the choices! Please try again.");
-        whatAnime();
+    let didntPick1 = true;
+    while(didntPick1){
+        let urAnime = prompt("Of Naruto, One Piece, and Bleach, which anime is your favorite?");
+        if (urAnime == "Naruto"){
+            document.write("Interesting. Are you an imperialist? Lol jk, it is good af.");
+            didntPick1 = false;
+        }
+        else if (urAnime == "One Piece") {
+            document.write("Nice! I commend you for watching all of those episdoes");
+            didntPick1 = false;
+        }
+        else if (urAnime == "Bleach") {
+            document.write("Personally I don't get the hype, but the soundtrack is fuego");
+            didntPick1 = false;
+        }
+        else {
+            alert("Sorry that was not one of the choices! Please try again.");
+        }
     }
 }
+
+
 //let favAnime = "";
 //function whatAnime() {
 //    let favAnime = prompt ("What's your favorite anime?")\
